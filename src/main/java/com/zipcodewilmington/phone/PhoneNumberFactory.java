c3f0bfd6caa539004f6a1c6574dcfdc58b9d46f2
JadeBolds <89998780+JadeBolds@users.noreply.github.com>
2021-10-22 12:05:34 -0400

    Homework edits
    10/22/21

--- src/main/java/com/zipcodewilmington/phone/PhoneNumberFactory.java
+++ src/main/java/com/zipcodewilmington/phone/PhoneNumberFactory.java
@@ -2,11 +2,12 @@ package com.zipcodewilmington.phone;
 
 import com.zipcodewilmington.exceptions.InvalidPhoneNumberFormatException;
 
+import java.util.ArrayList;
+import java.util.Random;
+
+
 import java.util.logging.Logger;
 
-/**
- * Created by leon on 5/1/17.
- */
 public final class PhoneNumberFactory {
     private static final Logger logger = Logger.getGlobal();
 
@@ -19,15 +20,34 @@ public final class PhoneNumberFactory {
      * @param phoneNumberCount - number of PhoneNumber objects to instantiate
      * @return array of randomly generated PhoneNumber objects
      */ //TODO - Implement logic
-    public static PhoneNumber[] createRandomPhoneNumberArray(int phoneNumberCount) {
-        return null;
+    public static PhoneNumber[] createRandomPhoneNumberArray(int phoneNumberCount) throws InvalidPhoneNumberFormatException {
+        PhoneNumber[] arrayOfPhoneNumbers = new PhoneNumber[phoneNumberCount];
+        for(int j = 0; j < phoneNumberCount; j++){
+            arrayOfPhoneNumbers[j] = createRandomPhoneNumber();
+        }
+        return arrayOfPhoneNumbers;
     }
 
     /**
      * @return an instance of PhoneNumber with randomly generated phone number value
      */ //TODO - Implement logic
-    public static PhoneNumber createRandomPhoneNumber() {
-        return createPhoneNumberSafely(-1, -1, -1);
+    public static PhoneNumber createRandomPhoneNumber() throws InvalidPhoneNumberFormatException {
+        //Random rand = new Random();
+        int areaCodeMin = 100;
+        int areaCodeMax = 999;
+
+        int centralOfficeMin = 0;
+        int centralOfficeMax = 999;
+
+        int phoneLineMin = 1000;
+        int phoneLineMax = 9999;
+
+        int areaCode = (int)(Math.random()*(areaCodeMax-areaCodeMin+1)+areaCodeMin);
+        int centralOfficeCode = (int)(Math.random()*(centralOfficeMax-centralOfficeMin+1)+centralOfficeMin);
+        int phoneLineCode = (int)(Math.random()*(phoneLineMax-phoneLineMin+1)+phoneLineMin);
+
+
+        return createPhoneNumberSafely(areaCode, centralOfficeCode, phoneLineCode);
     }
 
 
@@ -37,8 +57,21 @@ public final class PhoneNumberFactory {
      * @param phoneLineCode     - 4 digit code
      * @return a new phone number object
      */ //TODO - if input is valid, return respective PhoneNumber object, else return null
-    public static PhoneNumber createPhoneNumberSafely(int areaCode, int centralOfficeCode, int phoneLineCode) {
-        return createPhoneNumber(null);
+    public static PhoneNumber createPhoneNumberSafely(int areaCode, int centralOfficeCode, int phoneLineCode) throws InvalidPhoneNumberFormatException {
+        String areaCodeString = String.valueOf(areaCode);
+        String centralOfficeCodeString = String.valueOf(centralOfficeCode);
+        String phoneLineCodeString = String.valueOf(phoneLineCode);
+
+        String phoneNumberString = areaCodeString + centralOfficeCodeString + phoneLineCodeString;
+
+        try {
+            createPhoneNumber(phoneNumberString);
+        }
+        catch(InvalidPhoneNumberFormatException e) {
+            return null;
+        }
+
+        return createPhoneNumber(phoneNumberString);
     }
 
     /**
@@ -46,7 +79,10 @@ public final class PhoneNumberFactory {
      * @return a new phone number object
      * @throws InvalidPhoneNumberFormatException - thrown if phoneNumberString does not match acceptable format
      */ // TODO - Add throws statement to method signature
-    public static PhoneNumber createPhoneNumber(String phoneNumberString) {
-        return null;
+    public static PhoneNumber createPhoneNumber(String phoneNumberString) throws InvalidPhoneNumberFormatException {
+//        if(phoneNumberString != "(###)-###-####"){
+//            throw new InvalidPhoneNumberFormatException();
+//        }
+        return new PhoneNumber(phoneNumberString);
     }
 }
